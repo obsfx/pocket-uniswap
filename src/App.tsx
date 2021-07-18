90da35046c69dc36ae42584829b87f53d2f62add
obsfx <balandiomer@gmail.com>
2021-07-18 04:56:48 +0300

    Configure storybook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
