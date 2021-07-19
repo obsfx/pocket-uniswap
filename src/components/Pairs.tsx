7f247a8240f6d82a43592d7b2fd47d137c2a5482
obsfx <balandiomer@gmail.com>
2021-07-19 07:05:15 +0300

    Add Spinner.

--- src/components/Pairs.tsx
+++ src/components/Pairs.tsx
@@ -1,26 +1,15 @@
 import { useQuery } from '@apollo/client'
-import styled from 'styled-components'
-import { Pair } from './Pair'
 import { PairData, GET_PAIRS } from '../queries'
-
-const PairsWrapper = styled.div``
-
-const Loader = styled.div`
-  font-size: 1.6rem;
-  padding-top: 2rem;
-  text-transform: uppercase;
-  text-align: center;
-  font-weight: 500;
-  color: #a2a2a2;
-`
+import { Pair } from './Pair'
+import { Spinner } from './Spinner'
 
 export const Pairs: React.FC = () => {
   const { loading, data } = useQuery<PairData>(GET_PAIRS)
 
   return (
-    <PairsWrapper>
+    <>
       {loading ? (
-        <Loader>Loading...</Loader>
+        <Spinner />
       ) : (
         data &&
         data.pairs.map(({ id, token0, token1, reserveUSD, volumeUSD }) => (
@@ -33,6 +22,6 @@ export const Pairs: React.FC = () => {
           />
         ))
       )}
-    </PairsWrapper>
+    </>
   )
 }
